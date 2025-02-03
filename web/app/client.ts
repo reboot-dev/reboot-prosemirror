d03d30c9a67e153d7af43df956e53403bcd310a6
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2025-02-03 17:44:28 +0100

    [Release] Synchronize for release (#7)

--- web/app/client.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { RebootClient } from "@reboot-dev/reboot-react";
-
-// Need to instantiate `RebootClient` in a file that has "use client",
-// will update in a future release to remove this requirement.
-const client = new RebootClient("http://localhost:9991");
-
-export default client;
